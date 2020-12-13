776a0dd3e09dd4fc5a7fbe93a799969d26fdc9bd
caroline223 <carolineforrester22@gmail.com>
2020-12-12 21:51:14 -0500

    Updating the scraper

--- lib/favorite_cocktails/scraper.rb
+++ lib/favorite_cocktails/scraper.rb
@@ -4,25 +4,42 @@ require 'pry'
 
 
 
-class Scraper
+class FavoriteCocktails::Scraper
 
     BASE_URL = "https://makemeacocktail.com/"
 
-    def self.scrape_drinks
+    def self.scrape_index(url)
         page_url = 'https://makemeacocktail.com/recipes/100+Cocktails/'
 
-        drinks = Nokogiri::HTML(open(page_url))
-        cocktail_drinks = drinks.css("a")
-        cocktail_drinks.each do |cocktails|
-                hash = {
-                name: cocktails.css("h5").text.split, 
-                rating: cocktails.css() 
-        }        
+        favorite_cocktails = []
+        doc = Nokogiri::HTML(open(url))
+        drinks = doc.css()
+            binding.pry
+        drinks.each do |d|
+            drink = {}
+            drink[:name] = d.text
+            drink[:page_url] = d.attr("href")
+            favorite_cocktails << drink
         end
-        binding.pry
-        cocktail_description = description.css()
-        
-        FavoriteCocktails.new(cocktails)
+        favorite_cocktails
     end
+
+    
+    #def self.scrape_drinks
+        #page_url = 'https://makemeacocktail.com/recipes/100+Cocktails/'
+
+        #drinks = Nokogiri::HTML(open(page_url))
+        #cocktail_drinks = drinks.css("a")
+        #cocktail_drinks.each do |cocktails|
+                #hash = {
+                #name: cocktails.css("h5").text.split, 
+                #rating: cocktails.css() 
+        #}        
+        #end
+        #binding.pry
+        #cocktail_description = description.css()
+        
+        #FavoriteCocktails.new(cocktails)
+    #end
 
 end 
