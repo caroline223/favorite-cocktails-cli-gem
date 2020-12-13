cd00843bb52b0bd1be8ce20b3dc146472aee6108
caroline223 <carolineforrester22@gmail.com>
2020-12-13 10:33:11 -0500

    Updating the scraper

--- lib/favorite_cocktails/scraper.rb
+++ lib/favorite_cocktails/scraper.rb
@@ -6,20 +6,20 @@ require 'pry'
 
 class FavoriteCocktails::Scraper
 
-   # def self.scrape_index(url)
-        #url = 'https://www.townandcountrymag.com/leisure/drinks/g13092298/popular-bar-drinks-to-order/'
+   def self.scrape_index(url)
+        url = 'https://www.townandcountrymag.com/leisure/drinks/g13092298/popular-bar-drinks-to-order/'
 
-        #favorite_cocktails = []
-        #doc = Nokogiri::HTML(open(url))
-        #drinks =  doc.css("div .listicle-slide-hed").text.gsub("\n"," ").gsub("\t","").strip
-        #drinks.each do |d|
-            #drink = {}
-            #drink[:name] = d.text 
-            #drink[:page_url] = d.attr("href")
-            #favorite_cocktails << drink
-        #end
-        #favorite_cocktails
-    #end
+        favorite_cocktails = []
+        doc = Nokogiri::HTML(open(url))
+        drinks =  doc.css("div .listicle-slide-hed").text.gsub("\n"," ").gsub("\t","").strip
+        drinks.each do |d|
+            drink = {}
+            drink[:name] = d.text 
+            drink[:page_url] = d.attr("href")
+            favorite_cocktails << drink
+        end
+        favorite_cocktails
+    end
 
     
     def self.scrape_info(url)
@@ -27,11 +27,11 @@ class FavoriteCocktails::Scraper
 
         drink = {}
         doc = Nokogiri::HTML(open(url))
+        drink[:description] = doc.css("div .listicle-slide-dek p:first-child").text.split(":")
+        drink[:image] = doc.css("div span picture img").attr("src").value 
+        drink[:ingredients] = doc.css("div p:nth-child(2)").text.split("-")
+        drink[:instructions] = doc.css("div .listicle-slide-dek p em").text.split(".")
         binding.pry
-        #drink[:description] = doc.css("div .listicle-slide-dek p:first-child").text.split(":")
-        #drink[:image] = doc.css("div span picture img").attr("src").value 
-        #drink[:ingredients] = doc.css("div p:nth-child(2)").text.split("-")
-        #drink[:instructions] = doc.css("div .listicle-slide-dek p em").text.split(".")
         #drink
     end
         
